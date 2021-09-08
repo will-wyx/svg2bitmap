e17b3c8c3b97dcf1ec082ebad93931975a1e3653
will-wyx <wyx_msn@hotmail.com>
2021-09-08 16:20:15 +0800

    feat(*): 实现转换功能

---
+++ svg2bitmap.js
@@ -0,0 +1,32 @@
+function svg2bitmap (svgDom, type, cb) {
+  // 创建 svg 容器
+  const container = document.createElement('div')
+  // 将 svg 元素添加到容器中
+  container.appendChild(svgDom.cloneNode(true))
+  // 取得 svg 元素 html 文本
+  const svgHtml = container.innerHTML
+  // 生成 svg 格式的 dataURL
+  const svgDataURL = `data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(svgHtml)))}`
+
+  // 创建 canvas 元素
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+  canvas.width = svgDom.clientWidth
+  canvas.height = svgDom.clientHeight
+
+  // 创建图片元素
+  const image = new Image()
+  image.onload = () => {
+    try {
+      ctx.drawImage(image, 0, 0)
+      const dataURL = canvas.toDataURL(type)
+      cb(dataURL)
+    } catch (e) {
+
+    }
+  }
+  // 绑定图片 src
+  image.src = svgDataURL
+
+  cb('dataURL')
+}
